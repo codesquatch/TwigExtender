cab96d247f09cbc6787690876af8a66c91df8988
Patrick Sweeney <patrick.sweeney@fortyfour.com>
2019-07-26 12:21:06 -0400

    revamp twigextender

---
+++ src/TwigExtender.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace CodeSquatch;
+
+use Twig\Environment;
+use Twig_Extension;
+use Twig_ExtensionInterface;
+use Twig_SimpleFilter;
+use Twig_SimpleFunction;
+
+
+class TwigExtender extends Twig_Extension implements Twig_ExtensionInterface {
+
+  public static function returnParam($param) {
+    return $param;
+  }
+
+  public function initRuntime(Environment $environment) {
+    // TODO: Implement initRuntime() method.
+  }
+
+  public static function drupalAttributes(array $attributes) {
+    foreach ($attributes as $attribute => &$data) {
+      $data = implode(' ', (array) $data);
+      $data = $attribute . '="' . check_plain($data) . '"';
+    }
+    return $attributes ? ' ' . implode(' ', $attributes) : '';
+  }
+
+  function getFunctions() {
+    return [
+      new Twig_SimpleFunction('drupal_attributes', [$this, 'drupalAttributes']),
+    ];
+  }
+
+  function getGlobals() {
+    return [];
+  }
+
+  function getName() {
+    return 'CodeSquatch\TwigExtender';
+  }
+
+}
